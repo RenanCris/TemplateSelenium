276ee05eb6e4a7410bea11592d640aa599ad2361
RenanCrisConceicao <renancris21@gmail.com>
2020-03-25 12:10:51 -0300

    setup inicial

---
+++ Selenium/Selenium/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://github.com/RenanCris/TemplateSelenium");
+            driver.Manage().Window.Maximize();
+
+            Console.ReadKey();
+            driver.Dispose();
+        }
+    }
+}
